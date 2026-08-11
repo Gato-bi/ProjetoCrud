f05aead56333ee6e0a19f4357f34d29fa2d88dd5
Leonardo <gato.raivoso.daniel@gmail.com>
2026-08-11 09:10:15 -0300

    Mudando de int para string

--- Models/MED_AGENDAMENTO.cs
+++ Models/MED_AGENDAMENTO.cs
@@ -11,7 +11,7 @@ namespace ProjetoCrud.Models
         public int ID_MED_AGENDAMENTO { get; set; }
         public int ID_PAC_RG_CIN { get; set; }
         public int ID_MED_TAB_AGENDA_PERIODO { get; set; }
-        public int  ID_MED_CRM { get; set; }
+        public string ID_MED_CRM { get; set; }
         public TimeSpan MED_AGENDAMENTO_HORARIO { get; set; }
         public DateTime MED_AGENDAMENTO_DATA { get; set; }
         public int ID_MED_AGENDAMENTO_STATUS { get; set; }
